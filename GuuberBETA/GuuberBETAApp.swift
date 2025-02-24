5ffe87999f90a14eac3920ded37b79eb5a8c1da7
Gabriel Holte <holte243@umn.edu>
2025-02-24 13:30:45 -0600

    Initial Commit

---
+++ GuuberBETA/GuuberBETAApp.swift
@@ -0,0 +1,32 @@
+//
+//  GuuberBETAApp.swift
+//  GuuberBETA
+//
+//  Created by Gabe Holte on 2/24/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GuuberBETAApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ GuuberBETATests/GuuberBETATests.swift
@@ -0,0 +1,16 @@
+//
+//  GuuberBETATests.swift
+//  GuuberBETATests
+//
+//  Created by Gabe Holte on 2/24/25.
+//
+
+import Testing
+
+struct GuuberBETATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

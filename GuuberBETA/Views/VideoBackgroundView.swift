300736c6c215e084338e931746d76f566b3f2ba3
Gabriel Holte <holte243@umn.edu>
2025-03-17 22:51:06 -0500

    attmpt : 1 fallback error
    crashing of device when trying to be previewed

--- GuuberBETA/Views/VideoBackgroundView.swift
+++ GuuberBETA/Views/VideoBackgroundView.swift
@@ -5,34 +5,63 @@ struct VideoBackgroundView: View {
     let darkModeVideoName: String
     let lightModeVideoName: String
     @Environment(\.colorScheme) var colorScheme
+    @State private var player: AVPlayer?
     
     var body: some View {
         GeometryReader { geometry in
-            VideoPlayer(player: createPlayer())
-                .edgesIgnoringSafeArea(.all)
-                .frame(width: geometry.size.width, height: geometry.size.height)
-                .disabled(true) // Disable interaction with the video
-                .onAppear {
-                    createPlayer().play()
+            ZStack {
+                // Fallback gradient background
+                if player == nil {
+                    backgroundGradient
+                        .edgesIgnoringSafeArea(.all)
+                } else {
+                    VideoPlayer(player: player!)
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .disabled(true)
                 }
+            }
+        }
+        .onAppear {
+            setupPlayer()
+        }
+        .onChange(of: colorScheme) { _ in
+            setupPlayer()
         }
     }
     
-    private func createPlayer() -> AVPlayer {
+    private var backgroundGradient: some View {
+        let colors: [Color] = colorScheme == .dark ?
+            [Color.black, Color(red: 0.1, green: 0.1, blue: 0.3)] :
+            [Color.white, Color(red: 0.9, green: 0.9, blue: 1.0)]
+        
+        return LinearGradient(
+            gradient: Gradient(colors: colors),
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+    }
+    
+    private func setupPlayer() {
         let videoName = colorScheme == .dark ? darkModeVideoName : lightModeVideoName
-        guard let path = Bundle.main.path(forResource: videoName, of: "mp4") else {
-            fatalError("Could not find video file: \(videoName)")
+        guard let path = Bundle.main.path(forResource: videoName, ofType: "mp4") else {
+            print("Could not find video file: \(videoName).mp4")
+            player = nil
+            return
         }
-        let player = AVPlayer(url: URL(fileURLWithPath: path))
-        player.actionAtItemEnd = .none
+        
+        let newPlayer = AVPlayer(url: URL(fileURLWithPath: path))
+        newPlayer.actionAtItemEnd = .none
         NotificationCenter.default.addObserver(
             forName: .AVPlayerItemDidPlayToEndTime,
-            object: player.currentItem,
+            object: newPlayer.currentItem,
             queue: .main) { _ in
-                player.seek(to: .zero)
-                player.play()
+                newPlayer.seek(to: .zero)
+                newPlayer.play()
             }
-        return player
+        
+        player = newPlayer
+        player?.play()
     }
 }
 
